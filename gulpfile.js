4d5c8c13a592620b70d80fe140dc22e24fbdd251
Maciej Bukowski <ma2ciek@gmail.com>
2017-04-01 20:43:41 +0200

    Added simple server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const {
-	FuseBox,
-	TypeScriptHelpers
+    FuseBox,
+    TypeScriptHelpers
 } = require( "fuse-box" );
 const gulp = require( 'gulp' );
 const tslint = require( 'gulp-tslint' );
@@ -10,45 +10,38 @@ const cp = require( 'child_process' );
 
 // Create FuseBox Instance
 const fuseBox = new FuseBox( {
-	homeDir: "src/",
-	sourceMaps: true,
-	cache: true,
-	outFile: "./build/dist.js",
-	plugins: [ TypeScriptHelpers ],
+    homeDir: "src/",
+    sourceMaps: true,
+    cache: true,
+    outFile: "./build/dist.js",
+    plugins: [ TypeScriptHelpers ],
 } );
 
 gulp.task( 'lint', () =>
-	gulp.src( 'src/**/*.ts' )
-	.pipe( tslint( {
-		formatter: 'verbose',
-	} ) )
-	.pipe( tslint.report( {
-		emitError: false,
-	} ) )
+    gulp.src( 'src/**/*.ts' )
+    .pipe( tslint( {
+        formatter: 'verbose',
+    } ) )
+    .pipe( tslint.report( {
+        emitError: false,
+    } ) )
 );
 
 gulp.task( "build", () => {
-	return fuseBox.bundle( ">index.ts" );
+    return fuseBox.bundle( ">index.ts" );
 } );
 
 gulp.task( 'test', () => {
-	return runTests();
+    return runTests();
 } );
 
-gulp.task( 'default', [ 'build', 'lint', 'watch', 'server' ] );
-
-gulp.task('server', () => {
-	const httpServer = cp.spawn( 'http-server', [ '.' ] );
-	httpServer.stdout.on( 'data', data => {
-		console.log( data.toString() );
-	} );
-} );
+gulp.task( 'default', [ 'build', 'lint', 'watch' ] );
 
 gulp.task( 'watch', () => {
-	return gulp.watch( [
-		'src/**/**', 'test/**/**',
-	], [
-		'build',
-		'lint',
-	] )
+    return gulp.watch( [
+        'src/**/**', 'test/**/**',
+    ], [
+        'build',
+        'lint',
+    ] )
 } );
